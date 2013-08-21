9076f53b2a8e7e7688613e6b573c19b36ef20396
tkeble <thomas.keble@baesystemsdetica.com>
2013-08-21 18:55:21 +0100

    changing separator in JTreePathFinder and making class public so we can have access to the separator

--- fest-swing/src/main/java/org/fest/swing/driver/JTreePathFinder.java
+++ fest-swing/src/main/java/org/fest/swing/driver/JTreePathFinder.java
@@ -34,97 +34,105 @@ import org.fest.swing.exception.LocationUnavailableException;
  * 
  * @author Alex Ruiz
  */
-class JTreePathFinder {
-  // TODO TEST
-  private static final String SEPARATOR = "/";
+public class JTreePathFinder {
+    // TODO TEST
+    public static final String SEPARATOR = "/u0x1f";
 
-  private JTreeCellReader cellReader;
-  private String separator;
+    private JTreeCellReader cellReader;
+    private String separator;
 
-  JTreePathFinder() {
-    cellReader(new BasicJTreeCellReader());
-    separator(SEPARATOR);
-  }
-
-  @RunsInCurrentThread
-  @Nonnull TreePath findMatchingPath(@Nonnull JTree tree, @Nonnull String path) {
-    String[] pathStrings = splitPath(path);
-    TreeModel model = tree.getModel();
-    List<Object> newPathValues = newArrayList();
-    Object node = model.getRoot();
-    int pathElementCount = pathStrings.length;
-    for (int stringIndex = 0; stringIndex < pathElementCount; stringIndex++) {
-      String pathString = pathStrings[stringIndex];
-      Object match = null;
-      if (stringIndex == 0 && tree.isRootVisible()) {
-        if (!pathString.equals(value(tree, node))) {
-          throw pathNotFound(path);
-        }
-        newPathValues.add(node);
-        continue;
-      }
-      int childCount = model.getChildCount(node);
-      for (int childIndex = 0; childIndex < childCount; childIndex++) {
-        Object child = model.getChild(node, childIndex);
-        if (pathString.equals(value(tree, child))) {
-          if (match != null) {
-            throw multipleMatchingNodes(pathString, value(tree, node));
-          }
-          match = child;
-        }
-      }
-      if (match == null) {
-        throw pathNotFound(path);
-      }
-      newPathValues.add(match);
-      node = match;
+    JTreePathFinder() {
+        cellReader(new BasicJTreeCellReader());
+        separator(SEPARATOR);
     }
-    return new TreePath(newPathValues.toArray());
-  }
 
-  private @Nonnull LocationUnavailableException pathNotFound(@Nonnull String path) {
-    throw new LocationUnavailableException(String.format("Unable to find path %s", quote(path)));
-  }
-
-  private @Nonnull String[] splitPath(@Nonnull String path) {
-    List<String> result = newArrayList();
-    int separatorSize = separator.length();
-    int index = 0;
-    int pathSize = path.length();
-    while (index < pathSize) {
-      int separatorPosition = path.indexOf(separator, index);
-      if (separatorPosition == -1) {
-        separatorPosition = pathSize;
-      }
-      result.add(path.substring(index, separatorPosition));
-      index = separatorPosition + separatorSize;
+    @RunsInCurrentThread
+    @Nonnull
+    TreePath findMatchingPath(@Nonnull final JTree tree, @Nonnull final String path) {
+        String[] pathStrings = splitPath(path);
+        TreeModel model = tree.getModel();
+        List<Object> newPathValues = newArrayList();
+        Object node = model.getRoot();
+        int pathElementCount = pathStrings.length;
+        for (int stringIndex = 0; stringIndex < pathElementCount; stringIndex++) {
+            String pathString = pathStrings[stringIndex];
+            Object match = null;
+            if (stringIndex == 0 && tree.isRootVisible()) {
+                if (!pathString.equals(value(tree, node))) {
+                    throw pathNotFound(path);
+                }
+                newPathValues.add(node);
+                continue;
+            }
+            int childCount = model.getChildCount(node);
+            for (int childIndex = 0; childIndex < childCount; childIndex++) {
+                Object child = model.getChild(node, childIndex);
+                if (pathString.equals(value(tree, child))) {
+                    if (match != null) {
+                        throw multipleMatchingNodes(pathString, value(tree, node));
+                    }
+                    match = child;
+                }
+            }
+            if (match == null) {
+                throw pathNotFound(path);
+            }
+            newPathValues.add(match);
+            node = match;
+        }
+        return new TreePath(newPathValues.toArray());
     }
-    return result.toArray(new String[result.size()]);
-  }
 
-  private @Nonnull LocationUnavailableException multipleMatchingNodes(@Nonnull String matchingText,
-      @Nullable Object parentText) {
-    String msg = String.format("There is more than one node with value '%s' under", matchingText, quote(parentText));
-    throw new LocationUnavailableException(msg);
-  }
+    private @Nonnull
+    LocationUnavailableException pathNotFound(@Nonnull final String path) {
+        throw new LocationUnavailableException(String.format("Unable to find path %s", quote(path)));
+    }
 
-  private @Nullable String value(@Nonnull JTree tree, @Nullable Object modelValue) {
-    return cellReader.valueAt(tree, modelValue);
-  }
+    private @Nonnull
+    String[] splitPath(@Nonnull final String path) {
+        List<String> result = newArrayList();
+        int separatorSize = this.separator.length();
+        int index = 0;
+        int pathSize = path.length();
+        while (index < pathSize) {
+            int separatorPosition = path.indexOf(this.separator, index);
+            if (separatorPosition == -1) {
+                separatorPosition = pathSize;
+            }
+            result.add(path.substring(index, separatorPosition));
+            index = separatorPosition + separatorSize;
+        }
+        return result.toArray(new String[result.size()]);
+    }
 
-  @Nonnull String separator() {
-    return separator;
-  }
+    private @Nonnull
+    LocationUnavailableException multipleMatchingNodes(@Nonnull final String matchingText,
+            @Nullable final Object parentText) {
+        String msg = String
+                .format("There is more than one node with value '%s' under", matchingText, quote(parentText));
+        throw new LocationUnavailableException(msg);
+    }
 
-  void separator(@Nonnull String newSeparator) {
-    separator = newSeparator;
-  }
+    private @Nullable
+    String value(@Nonnull final JTree tree, @Nullable final Object modelValue) {
+        return this.cellReader.valueAt(tree, modelValue);
+    }
 
-  void cellReader(@Nonnull JTreeCellReader newCellReader) {
-    cellReader = newCellReader;
-  }
+    @Nonnull
+    String separator() {
+        return this.separator;
+    }
 
-  @Nonnull JTreeCellReader cellReader() {
-    return cellReader;
-  }
+    void separator(@Nonnull final String newSeparator) {
+        this.separator = newSeparator;
+    }
+
+    void cellReader(@Nonnull final JTreeCellReader newCellReader) {
+        this.cellReader = newCellReader;
+    }
+
+    @Nonnull
+    JTreeCellReader cellReader() {
+        return this.cellReader;
+    }
 }
